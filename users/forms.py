c4bd04b469641ed162999a2ba0ee998e98b5fc74
raulbreton <raul.elizalde.breton@gmail.com>
2023-11-30 13:25:13 -0600

    Register Backend and FrontEnd Completed

---
+++ users/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+CustomUser = get_user_model()
+
+class SignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('email', 'password1', 'password2', 'is_candidate', 'is_employer')
+
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = 'ejemplo@outlook.com'
+        self.fields['email'].label = ''
+        self.fields['email'].help_text = ''
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Contraseña'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Tu contraseña no puede ser demasiado similar a tu otra información personal.</li><li>Tu contraseña debe contener al menos 8 caracteres.</li><li>Tu contraseña no puede ser una contraseña comúnmente utilizada.</li><li>Tu contraseña no puede ser completamente numérica.</li></ul>'
+
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirma la Contraseña'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if CustomUser.objects.filter(email=email).exists():
+            raise forms.ValidationError("Este correo electrónico ya está registrado. Por favor, utiliza otro correo.")
+        return email
--- users/views.py
+++ users/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from .forms import SignUpForm
 
-# Create your views here.
+"""def register_user(request):
+    form = SignUpForm
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+            is_employer = form.cleaned_data['is_employer']
+            is_candidate = form.cleaned_data['is_candidate']
+            #Log in user
+            user = authenticate(username=email,password=password)
+            login(request,user)
+            #messages.success(request, 'Te has registrado con éxito!')
+            
+            if is_employer == True:
+                print("Employers")
+                #return redirect('employers_home')
+            else:
+                print("Candidates")
+                #return redirect('candidates_home')
+        else:
+            return render(request, "register.html", {'form':form})
+    return render(request, "register.html", {'form':form})"""
+def register_user(request):
+    form = SignUpForm()
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        account_type = request.POST.get('account_type')
+
+        if form.is_valid() and account_type in ['Candidato', 'Empleador']:
+            form.save()
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=email, password=password)
+            login(request, user)
+
+            if account_type == 'Empleador':
+                form.instance.is_employer = True
+                form.instance.is_candidate = False
+                form.save()
+                #messages.success(request, 'Te has registrado como empleador con éxito!')
+                #return redirect('employers_home')
+            elif account_type == 'Candidato':
+                form.instance.is_employer = False
+                form.instance.is_candidate = True
+                form.save()
+                #messages.success(request, 'Te has registrado como candidato con éxito!')
+                #return redirect('candidates_home')
+        else:
+            return render(request, "register.html", {'form': form})
+
+    return render(request, "register.html", {'form': form})
